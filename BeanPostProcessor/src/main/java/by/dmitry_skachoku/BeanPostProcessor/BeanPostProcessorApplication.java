b43af7bd7e8c687af9aedd060da61bd04cb51015
Burukku1 <dima.dmitry06@gmail.com>
2024-06-06 17:22:14 +0300

    init

--- BeanPostProcessor/src/main/java/by/dmitry_skachoku/BeanPostProcessor/BeanPostProcessorApplication.java
+++ BeanPostProcessor/src/main/java/by/dmitry_skachoku/BeanPostProcessor/BeanPostProcessorApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeanPostProcessorApplication implements CommandLineRunner {
 
+
     @Autowired
     private TerminationQuoter quoter;
 
