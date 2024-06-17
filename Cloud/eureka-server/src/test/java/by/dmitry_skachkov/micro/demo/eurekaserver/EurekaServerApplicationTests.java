9c0b27c76773d33441f933af6e7a537d3cdb4110
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-06-17 11:28:03 +0300

    init

---
+++ Cloud/eureka-server/src/test/java/by/dmitry_skachkov/micro/demo/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dmitry_skachkov.micro.demo.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
