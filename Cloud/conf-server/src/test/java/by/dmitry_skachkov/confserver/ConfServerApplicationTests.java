91a1ccccd954ca419dd622ccf14d7218f8519b79
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-06-24 15:51:06 +0300

    add conf server to the application

---
+++ Cloud/conf-server/src/test/java/by/dmitry_skachkov/confserver/ConfServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dmitry_skachkov.confserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
